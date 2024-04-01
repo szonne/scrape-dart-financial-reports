0f244cf587bd3caa441e1158e9e12d3b7a8c196b
Seong Jong Han <seungjong0222@gmail.com>
2024-04-01 22:43:23 +0900

    Format

--- reports.py
+++ reports.py
@@ -220,9 +220,7 @@ class ReportCalculator:
             # 분기별 재무상태표, 손익계산서, 현금흐름표 정보 취합
             for report_type in ReportTypes:
                 target_df = report.get_target_type_data(report_type=report_type)
-                quarter_df = pd.concat([
-                    quarter_df, target_df
-                ])
+                quarter_df = pd.concat([quarter_df, target_df])
 
             if quarter_df.empty:
                 continue
