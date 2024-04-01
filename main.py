a04983b3511c5a3e0184e77744403c0f5bbfb695
Seong Jong Han <seungjong0222@gmail.com>
2024-04-01 23:48:49 +0900

    Add excel file format

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     target_corp = Corp.find_by_name(corp_list, "원텍")
 
     report_calculator = ReportCalculator(
-        corp_code=target_corp["corp_code"], is_connected=True, unit=Units.THOUSAND
+        corp_code=target_corp["corp_code"], is_connected=True, unit=Units.MILLION
     )
 
     start_year = 2022
@@ -22,6 +22,22 @@ if __name__ == "__main__":
         start_year=start_year, end_year=end_year, by_quarter=False
     )
 
-    with pd.ExcelWriter("원텍_단위_천원.xlsx", engine="openpyxl") as writer:
-        quarter_df.to_excel(writer, sheet_name="Quarter", index=False, header=True)
-        annual_df.to_excel(writer, sheet_name="Year", index=False, header=True)
+    if report_calculator.unit == Units.DEFAULT:
+        cell_width = 15
+    elif report_calculator.unit == Units.THOUSAND:
+        cell_width = 12
+    else:
+        cell_width = 9
+
+    filename = f"{target_corp['corp_name']}_{str(start_year)}_{str(end_year)}_unit_{report_calculator.unit.name.lower()}.xlsx"
+    with pd.ExcelWriter(filename, engine="xlsxwriter") as writer:
+        quarter_df.to_excel(writer, sheet_name="Quarter", index=False, header=True, float_format='#,###')
+        annual_df.to_excel(writer, sheet_name="Year", index=False, header=True, float_format='#,###')
+
+        workbook = writer.book
+        float_format = workbook.add_format({'num_format': '#,##0'})
+        for worksheet in [writer.sheets['Quarter'], writer.sheets['Year']]:
+            worksheet.set_column(0, 1000, width=cell_width, cell_format=float_format)
+
+
+
