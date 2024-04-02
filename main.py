419e86ac564776850a1ddde28b8081e2e8d3ee31
Seong Jong Han <seungjong0222@gmail.com>
2024-04-02 21:34:02 +0900

    Fix

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ if __name__ == "__main__":
     api_key = input("API Key를 입력하세요: ")
     print(f"{corp_name}의 사업보고서 데이터 취합을 준비 중입니다...")
     report_calculator = ReportCalculator(
-        corp_name=corp_name, is_connected=is_connected, unit=Units.THOUSAND, api_key=api_key
+        corp_name=corp_name,
+        is_connected=is_connected,
+        unit=Units.THOUSAND,
+        api_key=api_key,
     )
 
     print(f"{corp_name}의 사업보고서 데이터를 처리 중입니다...")
